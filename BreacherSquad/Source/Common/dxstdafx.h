4bd8e5ac09256e2ad32e3af23c858610d75b58bb
Mike D <mihaidmc81@gmail.com>
2020-12-16 11:47:44 +0100

    Loads new props file

--- BreacherSquad/Source/Common/dxstdafx.h
+++ BreacherSquad/Source/Common/dxstdafx.h
@@ -416,10 +416,11 @@ static const char* GOG_CLIENT_SECRET = "416a364b92edd3ac24d9d8830e670d03de80e277
 #include "constants/strings.h"
 #include "constants/font_ids.h"
 #include "constants/sounds.h"
-//exportate din editor
+// editor exported
 #include "constants/lvled_SPR.h"
 #include "constants/lights_SPR.h"
 #include "constants/actives_SPR.h"
+#include "constants/props_SPR.h"
 #include "constants/particles_SPR.h"
 #include "constants/igm_interface_SPR.h"
 #include "constants/bk_city_SPR.h"
---
+++ BreacherSquad/Source/Engine/Constants/props_SPR.h
@@ -0,0 +1,8 @@
+
+//Animation indexes (Idx)
+#define 	ANM_OBJECTS_SPR_MISC   0
+#define 	ANM_OBJECTS_SPR_CHARACTERS   1
+
+//Animation names (ID)
+#define 	ANMID_OBJECTS_SPR_MISC   "MISC"
+#define 	ANMID_OBJECTS_SPR_CHARACTERS   "CHARACTERS"
